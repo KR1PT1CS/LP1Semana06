3e0aa6a64274b4cb29c787ede1c9ced577339b8e
KR1PT1CS <mogos2625727@gmail.com>
2025-03-27 14:27:40 +0000

    Added the rest of the attributes to Enemy.cs + Final check on Chronos, all good

--- MyGame/Enemy.cs
+++ MyGame/Enemy.cs
@@ -4,33 +4,44 @@
  {
      public class Enemy
      {
-         
          private string name;
          private float health;
          private float shield;
+ 
          public Enemy(string name)
          {
              this.name = name;
+             SetName(name);
              health = 100;
              shield = 0;
          }
+ 
          public string GetName()
          {
              return name;
          }
+ 
+         public float GetHealth()
+         {
+             return health;
+         }
+ 
+         public float GetShield()
+         {
+             return shield;
+         }
+ 
+         public void SetName(string newName)
+         {
+             name = newName.Substring(0, Math.Min(8, newName.Length));
+         }
+ 
          public void TakeDamage(float damage)
          {
              shield -= damage;
-             if (shield < 0)
-             {
-                 float damageStillToInflict = -shield;
-                 shield = 0;
-                 health -= damageStillToInflict;
-                 if (health < 0) health = 0;
-             }
+         public void TakeDamage(float damage)
          }
      }
-     
- 
  }
-  
+ 
+ 
