f6ac018f690048ed85ad6d929930a5afec5b5b13
KR1PT1CS <mogos2625727@gmail.com>
2025-03-27 15:19:45 +0000

    Added the powerup counter to the Enemy.cs program

--- MyGame/Enemy.cs
+++ MyGame/Enemy.cs
@@ -4,10 +4,38 @@
  {
      public class Enemy
      {
-         private string name;
+      private string name;
          private float health;
          private float shield;
+         private static int totalPowerUpsCollected;
  
+         static Enemy()
+         {
+             totalPowerUpsCollected = 0;
+         }
+ 
+         public Enemy(string name)
+         {
+         public float GetShield()
+             return shield;
+         }
+ 
+         public static int GetTotalPowerUpsCollected()
+         {
+             return totalPowerUpsCollected;
+         }
+ 
+         public void SetName(string newName)
+         {
+             name = newName.Substring(0, Math.Min(8, newName.Length));
+         }
+ 
+         public void PickupPowerUp(PowerUp powerUp, float value)
+         {
+             totalPowerUpsCollected++;
+             if (powerUp == PowerUp.Health)
+             
+                 health = Math.Min(100, health + value);
          public Enemy(string name)
          {
              this.name = name;
@@ -17,7 +45,7 @@
          }
  
          public string GetName()
-         {
+         {  
              return name;
          }
  
@@ -52,7 +80,8 @@
              shield -= damage;
          public void TakeDamage(float damage)
          }
-     }
+      }
+    }
  }
  
  
